d57e7c384b5447e0195673ed99a2e016c36b3c27
kirka50 <75656501+kirka50@users.noreply.github.com>
2024-06-25 22:25:41 +0500

    dist add

--- dist/assets/ssrBoot-D3xrfyG9.js
+++
@@ -1,1 +0,0 @@
-import{G as t,K as e,b as s,aF as n}from"./index-BvamWhd6.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
