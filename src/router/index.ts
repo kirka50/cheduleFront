9e53b96efa8ba9de4ac820fbc3dee97fde46f08e
kirka50 <75656501+kirka50@users.noreply.github.com>
2024-06-25 22:52:36 +0500

    webhash history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory, createMemoryHistory, createWebHashHistory } from 'vue-router'
 import {routes} from "./routes";
 import {useUserStore} from "../store/user/model/userStore";
 const router = createRouter({
-    history: createWebHistory('/cheduleFront/'),
+    history: createWebHashHistory('/cheduleFront/'),
     routes,
 })
 router.beforeEach((to,from, next) => {
     const userStore = useUserStore()
     if (to.meta.requiresAuth && !userStore.isUserAuth) {
-
         console.log('da')
         next('/auth/login')
     } else (next())
