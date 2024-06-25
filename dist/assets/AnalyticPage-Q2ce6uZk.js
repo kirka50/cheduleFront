db0c55a2448c374f7beaa7d0f0293982ad96bc95
kirka50 <75656501+kirka50@users.noreply.github.com>
2024-06-25 21:11:50 +0500

    dist add

--- dist/assets/AnalyticPage-Q2ce6uZk.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-IewNzhfI.js";const e={name:"AnalyticPage"};function a(n,c,r,o,s,_){return" ПИСЬКОСОс "}const f=t(e,[["render",a]]);export{f as default};
---
+++ dist/assets/ssrBoot-CWzIX7z6.js
@@ -0,0 +1,1 @@
+import{G as t,K as e,b as s,aF as n}from"./index-Cgmj9m30.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
