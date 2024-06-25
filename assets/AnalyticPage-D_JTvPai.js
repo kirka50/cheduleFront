7a9344b7e665efff3b14823d54bc8d0edfef1aec
kirka50 <75656501+kirka50@users.noreply.github.com>
2024-06-25 21:37:21 +0500

    dist add

---
+++ assets/AnalyticPage-D_JTvPai.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-BST0vJPl.js";const e={name:"AnalyticPage"};function a(n,c,r,o,s,_){return" ПИСЬКОСОс "}const f=t(e,[["render",a]]);export{f as default};
---
+++ assets/ssrBoot-BDmQZmIu.js
@@ -0,0 +1,1 @@
+import{G as t,K as e,b as s,aF as n}from"./index-BST0vJPl.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
